a24456187ed621d57bf0139c5fada01c0d10eae9
Mariusz <mariusz.kardas@appgo.pl>
2021-09-23 02:27:13 +0200

    mainPageWork

--- src/resources/views/default/index.blade.php
+++ src/resources/views/default/index.blade.php
@@ -1,5 +1,20 @@
-@extends('layouts.app')
+@extends('layouts.default')
 
 @section('content')
-
+    <header class="header">
+        <div class="container">
+            <div class="header__content">
+                <nav class="nav">
+                    <ul>
+                        <li><a href="">Strona główna</a></li>
+                        <li><a href="">Aktualności</a></li>
+                        <li><a href="">Galeria</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <section class="start">
+        <img src="{{asset('images/header.svg')}}" class="start__image" alt="">
+    </section>
 @endsection
